2a1698205b8d5ea03d058526d9e6fccd9ca1c282
luane-loureiro <luachan@gmail.com>
2024-11-14 19:38:05 -0300

    add new directory Aula_02

---
+++ exercicios/Aula_02/operadores _comparacao.py
@@ -0,0 +1,10 @@
+a, b = 10, 3
+
+print (f"Igual: {a == b}") #false
+print (f"Difereça: {a != b}") #true
+print (f"Maior que: {a > b}") #true
+print (f"Menor que: {a < b}") #false
+print (f"Maior ou igual: {a >= b}") #true
+print (f"menor ou igual: {a <= b}") #false
+
+input("Pressione Enter para sair...")
---
+++ exercicios/Aula_02/operadores_Aritimeticos.py
@@ -0,0 +1,11 @@
+a, b = 10, 3
+
+print (f"Adição: {a + b}")
+print (f"Subtração: {a - b}")
+print (f"Multiplicação: {a * b}")
+print (f"Divisao: {a / b:.3f}")
+print (f"Divisao Inteira: {a // b}")
+print (f"Módulo (resto): {a % b}")
+print (f"Esponencição: {a ** b}")
+
+input("Pressione Enter para sair...")
