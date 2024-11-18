fe8e60a0d60725f68b77f2230ceab3974c0b6b84
luane-loureiro <luachan@gmail.com>
2024-11-17 23:49:35 -0300

    add aulas 3

---
+++ exercicios/Aula_03/maior_de_idade.py
@@ -0,0 +1,12 @@
+idade = int (input("Digite a Sua Idade: "))
+
+if idade >=18:
+    print("Você é Maior de idade")
+
+elif 12 <= idade < 18:
+    print ("Você é Adolecente")
+
+else:
+    print("Você é uma Criança")
+
+input("Pressione Enter para sair...")
---
+++ exercicios/Aula_03/modelo_machine_leran/model.py
@@ -0,0 +1,19 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+horas_estudo = np.array([1,2,3,4,5]).reshape(-1,1)
+notas = np.array([40,50,60,70,80])
+
+#modelo treina
+modelo = LinearRegression()
+modelo.fit(horas_estudo, notas)
+
+#porgunto ao usiários final
+horas = float(input("Digite o número de horas estudadas: "))
+
+#previsao
+nota_prevista = modelo.predict([[horas]])
+
+print (f"Com {horas} de estudo, a nota prevista é {nota_prevista[0]:.2f}")
+
+input("Pressione Enter para sair...")
